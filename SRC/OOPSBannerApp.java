e0ed3eebfae6b51480e633113d3d4e7051e947aa
Vagesh36 <vageshvageshpatil@gmail.com>
2026-02-28 23:24:40 +0530

    Implemented UC3 to PrintOOPSBanner

---
+++ SRC/OOPSBannerApp.java
@@ -0,0 +1,48 @@
+// HelloWorld.java
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " ***** ",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                " *     "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                "  ***  "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                "     * "));
+
+        System.out.println(String.join(" ",
+                " *   * ",
+                " *   * ",
+                " *     ",
+                " *   * "));
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " *     ",
+                "  ***  "));
+
+        System.out.println(String.join(" ",
+                "       ",
+                "       ",
+                "       ",
+                "       "));
+    }
+}
